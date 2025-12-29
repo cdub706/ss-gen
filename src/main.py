655ee3277fb0f7c641505e5af7e6d3742fc271ec
cdub706 <williams.chauncey@gmail.com>
2025-12-28 20:35:01 -0500

    Added HTMLNode class usage in main.py to demonstrate paragraph creation alongside existing TextNode functionality.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 def main():
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
+
+    node1 = HTMLNode("p", "This is a paragrapgh")
+    print(node1)
 
 if __name__ == "__main__":
     main()
